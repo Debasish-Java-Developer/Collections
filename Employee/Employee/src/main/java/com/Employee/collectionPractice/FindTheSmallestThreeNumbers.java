cfe11b1de2675726595f4dec32e5e724f7194358
Debasish Mohanta <219381354+Debasish-Java-Developer@users.noreply.github.com>
2025-12-30 12:36:55 +0530

    Find the smallest three numbers from the given array

---
+++ Employee/Employee/src/main/java/com/Employee/collectionPractice/FindTheSmallestThreeNumbers.java
@@ -0,0 +1,14 @@
+package com.Employee.collectionPractice;
+
+import java.util.Arrays;
+
+public class FindTheSmallestThreeNumbers {
+    public static void main(String[] args) {
+        int [] numbers ={1, 5, 3, 8, 0}; // Sample array of integers
+        Arrays.stream(numbers)
+                .boxed()
+                .sorted((a,b)-> a-b) // Sorting in ascending order
+                .limit(3)
+                .forEach(System.out::println);
+    }
+}
