8e5c611fa7a01781965b76dcf62216ea54f1db70
Debasish Mohanta <219381354+Debasish-Java-Developer@users.noreply.github.com>
2026-01-02 21:10:45 +0530

    Example of flatmap

---
+++ Employee/Employee/src/main/java/com/Employee/collectionPractice/FlatMapExample.java
@@ -0,0 +1,18 @@
+package com.Employee.collectionPractice;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class FlatMapExample {
+    public static void main(String[] args) {
+        List<List<String>> list = Arrays.asList(
+                Arrays.asList("Java", "Spring"),
+                Arrays.asList("Hibernate", "Microservice")
+        );
+        // flatMap converts multiple lists into one stream
+        List<String> result = list.stream()
+                .flatMap(innerList -> innerList.stream()) // many outputs → merged
+                .collect(Collectors.toList());
+        System.out.println(result);
+    }
+}
