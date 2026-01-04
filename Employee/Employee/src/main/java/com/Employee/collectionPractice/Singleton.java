a9b603a49b13014c12d7b0f51649e8e13abb516c
Debasish Mohanta <219381354+Debasish-Java-Developer@users.noreply.github.com>
2026-01-04 22:08:55 +0530

    Singleton program code

---
+++ Employee/Employee/src/main/java/com/Employee/collectionPractice/Singleton.java
@@ -0,0 +1,26 @@
+package com.Employee.collectionPractice;
+
+public class Singleton {
+    private static Singleton instance = new Singleton();
+    private Singleton() {
+        System.out.println("Singleton object created");
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Hello from Singleton");
+    }
+}
+
+class TestSingleton {
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        s1.showMessage();
+        System.out.println(s1 == s2); // true
+    }
+}
---
+++ Employee/Employee/src/main/java/com/Employee/collectionPractice/ThrowsExample.java
@@ -0,0 +1,10 @@
+package com.Employee.collectionPractice;
+
+public class ThrowsExample {
+    static void readFile() throws Exception {
+        System.out.println("Reading file");
+    }
+    public static void main(String[] args) throws Exception {
+        readFile();
+    }
+}
