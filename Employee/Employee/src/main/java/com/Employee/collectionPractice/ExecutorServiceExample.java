8169ef6b4210df7314732705facc5159264e3296
Debasish Mohanta <219381354+Debasish-Java-Developer@users.noreply.github.com>
2026-01-09 07:15:23 +0530

    Executor example

---
+++ Employee/Employee/src/main/java/com/Employee/collectionPractice/ExecutorServiceExample.java
@@ -0,0 +1,17 @@
+package com.Employee.collectionPractice;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceExample {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(() -> {
+            System.out.println("Task 1 executed by " + Thread.currentThread().getName());
+        });
+        executor.submit(() -> {
+            System.out.println("Task 2 executed by " + Thread.currentThread().getName());
+        });
+        executor.shutdown();
+    }
+}
